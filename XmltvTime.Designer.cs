20938ac9411c2c9394d58d6c6e99d9de3ecb91ea
CodeMonkey <84345037+BillOatmanWork@users.noreply.github.com>
2022-01-15 21:53:45 -0500

    Add files via upload
    Initial

---
+++ XmltvTime.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace XmltvTime
+{
+    partial class XmltvTime
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(XmltvTime));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtXmltv = new System.Windows.Forms.TextBox();
+            this.dtDate = new System.Windows.Forms.DateTimePicker();
+            this.dtTime = new System.Windows.Forms.DateTimePicker();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnUp = new System.Windows.Forms.Button();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Xmltv: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 244);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Local DateTime:";
+            // 
+            // txtXmltv
+            // 
+            this.txtXmltv.Location = new System.Drawing.Point(159, 53);
+            this.txtXmltv.Name = "txtXmltv";
+            this.txtXmltv.Size = new System.Drawing.Size(238, 22);
+            this.txtXmltv.TabIndex = 2;
+            // 
+            // dtDate
+            // 
+            this.dtDate.CustomFormat = "";
+            this.dtDate.Location = new System.Drawing.Point(170, 239);
+            this.dtDate.Name = "dtDate";
+            this.dtDate.Size = new System.Drawing.Size(259, 22);
+            this.dtDate.TabIndex = 3;
+            // 
+            // dtTime
+            // 
+            this.dtTime.CustomFormat = "";
+            this.dtTime.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dtTime.Location = new System.Drawing.Point(474, 239);
+            this.dtTime.Name = "dtTime";
+            this.dtTime.ShowUpDown = true;
+            this.dtTime.Size = new System.Drawing.Size(124, 22);
+            this.dtTime.TabIndex = 4;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(281, 318);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(101, 44);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnUp
+            // 
+            this.btnUp.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUp.Location = new System.Drawing.Point(159, 104);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(104, 81);
+            this.btnUp.TabIndex = 6;
+            this.btnUp.UseVisualStyleBackColor = true;
+            this.btnUp.Click += new System.EventHandler(this.btnUp_Click);
+            // 
+            // btnDown
+            // 
+            this.btnDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDown.Location = new System.Drawing.Point(293, 103);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(104, 82);
+            this.btnDown.TabIndex = 7;
+            this.btnDown.UseVisualStyleBackColor = true;
+            this.btnDown.Click += new System.EventHandler(this.btnDown_Click);
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCopy.Location = new System.Drawing.Point(426, 43);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(76, 37);
+            this.btnCopy.TabIndex = 8;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // XmltvTime
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(662, 407);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.btnDown);
+            this.Controls.Add(this.btnUp);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.dtTime);
+            this.Controls.Add(this.dtDate);
+            this.Controls.Add(this.txtXmltv);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "XmltvTime";
+            this.Text = "Xmltv Time";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtXmltv;
+        private System.Windows.Forms.DateTimePicker dtDate;
+        private System.Windows.Forms.DateTimePicker dtTime;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnUp;
+        private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.Button btnCopy;
+    }
+}
+
---
+++ XmltvTime.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XmltvTime
+{
+    public partial class XmltvTime : Form
+    {
+        public XmltvTime()
+        {
+            InitializeComponent();
+
+            btnDown.Text = char.ConvertFromUtf32(0x21D3);
+            btnUp.Text = char.ConvertFromUtf32(0x21D1);
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        /// <summary>
+        /// Human readable to XMLTV format
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnUp_Click(object sender, EventArgs e)
+        {
+            DateTime selectedDate = dtDate.Value;
+            DateTime selectedTime = dtTime.Value;
+
+            DateTime selected = selectedDate.Date + selectedTime.TimeOfDay;
+
+            txtXmltv.Text = selected.ToUniversalTime().ToString("yyyyMMddHHmm00 +0000");
+        }
+
+        /// <summary>
+        /// XMLTV format to human readable
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnDown_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DateTime selected = DateTime.ParseExact(txtXmltv.Text, "yyyyMMddHHmmss K", CultureInfo.InvariantCulture);
+                dtDate.Value = selected.ToLocalTime();
+                dtTime.Value = selected.ToLocalTime();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(txtXmltv.Text);
+        }
+    }
+}
